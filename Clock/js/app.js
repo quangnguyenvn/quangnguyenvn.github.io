cb0e2727d42cf7f3c2926bd93a0c4245494928d1
quangnguyenvn <quangnguyen@octech.com.vn>
2018-12-13 22:54:23 +0700

    #99
    complete

---
+++ Clock/js/app.js
@@ -0,0 +1,88 @@
+function moveHands() {
+    with(new Date()) {
+        h = 30 * (getHours() % 12 + getMinutes() / 60); // 30 degrees hour
+        m = 6 * getMinutes(); // 6 degrees every minute
+        s = 6 * getSeconds(); // 6 degrees every second
+        // setting the rotate CSS attribute to those degree values -->
+        document.getElementById('seconds').style.cssText = "-webkit-transform:rotate(" + s + "deg);";
+        document.getElementById('minutes').style.cssText = "-webkit-transform:rotate(" + m + "deg);";
+        document.getElementById('hours').style.cssText = "-webkit-transform:rotate(" + h + "deg);";
+
+        setTimeout(moveHands, 1000); // calling the function every second
+    }
+}
+
+window.onload = moveHands(); // making sure the function starts on load of webpage
+
+// NY time - Second_clock
+
+function moveHandsNY() {
+    let d = new Date();
+    let localTime = d.getTime();
+    let localOffset = d.getTimezoneOffset() * 60000;
+    let utc = localTime + localOffset;
+    let offset = -4;   
+    let newYork = utc + (3600000*offset);
+
+    with(new Date(newYork)) {
+        h = 30 * (getHours() % 12 + getMinutes() / 60); 
+        m = 6 * getMinutes(); 
+        s = 6 * getSeconds();
+        document.getElementById('second_seconds').style.cssText = "-webkit-transform:rotate(" + s + "deg);";
+        document.getElementById('second_minutes').style.cssText = "-webkit-transform:rotate(" + m + "deg);";
+        document.getElementById('second_hours').style.cssText = "-webkit-transform:rotate(" + h + "deg);";
+
+        setTimeout(moveHandsNY, 1000);
+    }
+}
+
+window.onload = moveHandsNY();
+
+// TOKYO time - third_clock
+
+function moveHandsTYO() {
+    let d = new Date();
+    let localTime = d.getTime();
+    let localOffset = d.getTimezoneOffset() * 60000;
+    let utc = localTime + localOffset;
+    let offset = 9;   
+    let tokyo = utc + (3600000*offset);
+
+    with(new Date(tokyo)) {
+        h = 30 * (getHours() % 12 + getMinutes() / 60); 
+        m = 6 * getMinutes();
+        s = 6 * getSeconds(); 
+        document.getElementById('third_seconds').style.cssText = "-webkit-transform:rotate(" + s + "deg);";
+        document.getElementById('third_minutes').style.cssText = "-webkit-transform:rotate(" + m + "deg);";
+        document.getElementById('third_hours').style.cssText = "-webkit-transform:rotate(" + h + "deg);";
+
+        setTimeout(moveHandsTYO, 1000); 
+    }
+}
+
+window.onload = moveHandsTYO(); 
+
+
+// BERLIN time - fourth_clock
+
+function moveHandsBER() {
+    let d = new Date();
+    let localTime = d.getTime();
+    let localOffset = d.getTimezoneOffset() * 60000;
+    let utc = localTime + localOffset;
+    let offset = 2;   
+    let berlin = utc + (3600000*offset);
+    
+    with(new Date(berlin)) {
+        h = 30 * (getHours() % 12 + getMinutes() / 60); 
+        m = 6 * getMinutes();
+        s = 6 * getSeconds(); 
+        document.getElementById('fourth_seconds').style.cssText = "-webkit-transform:rotate(" + s + "deg);";
+        document.getElementById('fourth_minutes').style.cssText = "-webkit-transform:rotate(" + m + "deg);";
+        document.getElementById('fourth_hours').style.cssText = "-webkit-transform:rotate(" + h + "deg);";
+
+        setTimeout(moveHandsBER, 1000); 
+    }
+}
+
+window.onload = moveHandsBER(); 
---
+++ Quiz project/js/question.js
@@ -0,0 +1,82 @@
+var questions = [
+{
+"question": "Năm 2017, thành phố nào của Việt Nam không nằm trong top 100 thành phố hấp dẫn nhất toàn cầu ?",
+"option1": "Hà Nội",
+"option2": "Đà Nẵng",
+"option3": "Hồ Chí Minh",
+"option4": "Hạ Long",
+"answer": "2"	
+},
+{
+"question": "Thành phố nào là thủ đô của nước Bỉ ?",
+"option1": "Brussels",
+"option2": "Munich",
+"option3": "Antwerp",
+"option4": "Stuttgart",
+"answer": "1"	
+},
+{
+"question": "Thành phố cảng Alexanderia thuộc quốc gia nào ?",
+"option1": "Ý",
+"option2": "Hy Lạp",
+"option3": "Ai Cập",
+"option4": "Cyprus",
+"answer": "3" 
+},
+{
+"question": "Santorini là một thị trấn du lịch nằm sát bên ?",
+"option1": "Biển",
+"option2": "Sườn dốc",
+"option3": "Núi lửa",
+"option4": "Tất cả các phương án trên!", 
+"answer": "4"
+},
+{
+"question": "Los Angeles nằm chính xác ở đâu ?",
+"option1": "Bắc California",
+"option2": "Nam California",
+"option3": "Arizona",
+"option4": "Utah", 
+"answer": "2"
+},
+{
+"question": "Nova Scotia thuộc quốc gia nào?",
+"option1": "Scotland",
+"option2": "Chile",
+"option3": "Canada",
+"option4": "Greenland", 
+"answer": "3"
+},
+{
+"question": "Năm 2017, Thành phố nào đông dân nhất thế giới ?",
+"option1": "Bắc Kinh, Trung Quốc",
+"option2": "Delhi, Ấn Độ",
+"option3": "Lagos, Nigeria",
+"option4": "Thượng Hải, Trung Quốc", 
+"answer": "4"
+},
+{
+"question": "Năm 2017, Thành phố nào có mức sống rẻ nhất thế giới ?",
+"option1": "Damascus, Syria",
+"option2": "Caracas, Venezuela",
+"option3": "Lagos, Nigeria",
+"option4": "Bucharest,  Romania", 
+"answer": "1"
+},
+{
+"question": "Thành phố nào lạnh nhất thế giới ?",
+"option1": "Oymyakon, Nga",
+"option2": "Yakutsk, Séc-Bi",
+"option3": "FairBank, Alaska",
+"option4": "Nuuk, Greenland", 
+"answer": "2"
+},
+{
+"question": "Thành phố Atlantis nằm ở vùng biển nào ?",
+"option1": "Bắc Đại Tây Dương",
+"option2": "Nam Đại Tây Dương",
+"option3": "Địa Trung Hải",
+"option4": "Không đáp án nào chính xác!", 
+"answer": "4"
+}
+]
